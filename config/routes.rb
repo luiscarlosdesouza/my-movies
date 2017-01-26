fdb4a62d3ed129f218f3070109e3b23493e4537b
ruby 34 <ruby.34@metralha.cec.ime.usp.br>
2017-01-26 21:40:16 -0200

    Initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
+#  get 'movies/index'
+  get 'movies' => "movies#index"
+
+  get 'movies/index'
+  get 'movies/new' => "movies#new"
+
+#  get 'movies/show'
+  get 'movies/:id' => "movies#show"
+
+  post 'movies' => "movies#create"
+
+  get 'movies/:id/edit' => "movies#edit"
+
+  put 'movies/:id' => "movies#update"
+
+
+
+  #get 'movies/create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
